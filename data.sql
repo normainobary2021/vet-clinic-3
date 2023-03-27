8db70f89885494119d351e4c2d6330bfc12f3d05
Norman Ainobushoborozi <87012757+normainobary2021@users.noreply.github.com>
2023-03-28 01:12:10 +0300

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,5 @@
+INSERT INTO animals VALUES 
+(1, 'Agumon', '2020-2-3', 0, TRUE, 10.23),
+(2, 'Gabumon', '2018-11-15', 2, TRUE, 8),
+(3, 'Pikachu', '2021-1-7', 1, FALSE, 15.04),
+(4, 'Devimon', '2017-5-12', 5, TRUE, 11);
